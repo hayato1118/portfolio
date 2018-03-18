4c2d8a79cdfb3135279f0779853384f359554ee8
hayato1118 <hayato.nakamura1118@gmail.com>
2018-03-18 04:37:27 +0000

    deleted

--- db/migrate/20180206051956_remove_product_id_from_product_tags.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProductIdFromProductTags < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :product_tags, :Product_id, :integer
-  end
-end
--- db/migrate/20180206052030_add_product_id_to_product_tags.rb
+++
@@ -1,5 +0,0 @@
-class AddProductIdToProductTags < ActiveRecord::Migration[5.1]
-  def change
-    add_column :product_tags, :product_id, :integer
-  end
-end
